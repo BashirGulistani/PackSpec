3d7dd47c1e82122058e59783ad1f0bed426fd318
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2026-01-31 23:37:16 -0500

    Update units.py

--- src/packspec/units.py
+++ src/packspec/units.py
@@ -14,3 +14,21 @@ class Dim:
 
 
 
+@dataclass(frozen=True)
+class Weight:
+    value: float
+    unit: str  # "lb" or "kg"
+
+
+def cm_to_in(x: float) -> float:
+    return x / 2.54
+
+
+def in_to_cm(x: float) -> float:
+    return x * 2.54
+
+
+def kg_to_lb(x: float) -> float:
+    return x * 2.2046226218
+
+
