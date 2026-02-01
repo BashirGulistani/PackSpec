57973ec4765a02e3b3e7ac42f59d6d9f4bf4fcfe
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2026-01-31 23:37:25 -0500

    Update units.py

--- src/packspec/units.py
+++ src/packspec/units.py
@@ -32,3 +32,27 @@ def kg_to_lb(x: float) -> float:
     return x * 2.2046226218
 
 
+
+def lb_to_kg(x: float) -> float:
+    return x / 2.2046226218
+
+
+def normalize_dim(dim: Dim, to_unit: str = "in") -> Dim:
+    if dim.unit == to_unit:
+        return dim
+    if dim.unit == "cm" and to_unit == "in":
+        return Dim(cm_to_in(dim.l), cm_to_in(dim.w), cm_to_in(dim.h), "in")
+    if dim.unit == "in" and to_unit == "cm":
+        return Dim(in_to_cm(dim.l), in_to_cm(dim.w), in_to_cm(dim.h), "cm")
+    return dim
+
+
+def normalize_weight(w: Weight, to_unit: str = "lb") -> Weight:
+    if w.unit == to_unit:
+        return w
+    if w.unit == "kg" and to_unit == "lb":
+        return Weight(kg_to_lb(w.value), "lb")
+    if w.unit == "lb" and to_unit == "kg":
+        return Weight(lb_to_kg(w.value), "kg")
+    return w
+
